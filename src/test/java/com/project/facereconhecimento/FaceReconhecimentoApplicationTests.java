aacffec5260e5b16dd4c4086ebe4986b40e9e6a2
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-09-27 21:40:46 -0300

    testes com mock

--- src/test/java/com/project/facereconhecimento/FaceReconhecimentoApplicationTests.java
+++ src/test/java/com/project/facereconhecimento/FaceReconhecimentoApplicationTests.java
@@ -1,13 +1,56 @@
 package com.project.facereconhecimento;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import com.project.facereconhecimento.controller.PessoaControler;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+
 class FaceReconhecimentoApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private MockMvc mockMvc;
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private PessoaControler pessoaControler;
+
+
+    @Before(value = "")
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(PessoaControler.class).build();
     }
+
+    @Test
+    public void responseHttpTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/pessoa/listagem?pageNumber=0&pageSize=2"))
+                .andExpect(MockMvcResultMatchers.status().is(200));
+    }
+
+    @Test
+    public void cadastrarTest() throws Exception {
+        // mockMvc.perform(MockMvcRequestBuilders
+        //         .post("/api/pessoa/create")
+        //         .contentType(MediaType.APPLICATION_JSON)
+        //         .content("{\"id_pesssoa\": \"100\" \"ativo\": \"1\", \"cpf\": \"12256131912\": \"data_nascimento\": \"2004-12-20\",\"nome\": \"felipe\",\"tipo\": \"1\"}")
+        //         .accept(MediaType.APPLICATION_JSON))
+        //         .andExpect(MockMvcResultMatchers.status().is(200));
+
+    }
+
+
 
 }
--- src/test/java/com/project/facereconhecimento/TestPessoa.java
+++ src/test/java/com/project/facereconhecimento/TestPessoa.java
@@ -1,33 +1,34 @@
 package com.project.facereconhecimento;
 
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import com.project.facereconhecimento.controller.PessoaControler;
+import javafx.application.Application;
 
 
 @SpringBootTest
-
-@WebAppConfiguration
-@MockBean
-@SpringJUnitConfig
+@ContextConfiguration(classes=Application.class)
+@WebMvcTest()
+@DataJpaTest
+@ExtendWith(MockitoExtension.class)
 public class TestPessoa {
 
     @Autowired
     private MockMvc mockMvc;
 
-
     @Test
-    public void testCadastrar() throws Exception{
-
-
+    void whenValidInput_thenReturns200() throws Exception {
+        // mockMvc.perform(MockMvcRequestBuilders
+        // .get("/api/pessoa/create")
+        // .contentType("application/json"))
+        // .andExpect(MockMvcResultMatchers.status().is(200));
     }
+
 }
