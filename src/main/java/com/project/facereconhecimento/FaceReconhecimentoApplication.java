2951c60f3a6a0c648e80ed9be30986c17b94f534
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-10-01 22:06:25 -0300

    configuration class for working files

--- src/main/java/com/project/facereconhecimento/FaceReconhecimentoApplication.java
+++ src/main/java/com/project/facereconhecimento/FaceReconhecimentoApplication.java
@@ -1,9 +1,14 @@
 package com.project.facereconhecimento;
 
+import com.project.facereconhecimento.properties.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class FaceReconhecimentoApplication {
 
     public static void main(String[] args) {
