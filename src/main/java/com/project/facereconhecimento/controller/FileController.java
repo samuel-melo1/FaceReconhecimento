cfdab5f278f84842fe1bb4a99e80e9667fb0eb5c
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-10-12 20:25:36 -0300

    saving cpf as photos name

--- src/main/java/com/project/facereconhecimento/controller/FileController.java
+++ src/main/java/com/project/facereconhecimento/controller/FileController.java
@@ -1,20 +1,14 @@
 package com.project.facereconhecimento.controller;
 
-import com.project.facereconhecimento.model.Pessoa;
-import org.apache.tomcat.util.json.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import com.project.facereconhecimento.payload.UploadFileResponse;
 import com.project.facereconhecimento.service.FileStorageService;
 
-import java.beans.BeanProperty;
-import java.util.List;
 
 
 @RestController
@@ -26,12 +20,11 @@ public class FileController {
     @Autowired
     private FileStorageService fileStorageService;
     
-    @PostMapping("/uploadFile")
-    public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) {
-        String fileName = fileStorageService.storeFile(file);
+    @PostMapping("/uploadFile/{cpf}")
+    public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file, @PathVariable String cpf) {
+        String fileName = fileStorageService.storeFile(file, cpf);
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
 }
     
 
--- src/main/java/com/project/facereconhecimento/service/FileStorageService.java
+++ src/main/java/com/project/facereconhecimento/service/FileStorageService.java
@@ -33,38 +33,19 @@ public class FileStorageService {
         }
     }
 
-    public String storeFile(MultipartFile file) {
-        // Normalize file name
-        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+    public String storeFile(MultipartFile file, String cpf) {
+        String fileName = StringUtils.cleanPath(cpf + ".png");
 
         try {
-            // Check if the file's name contains invalid characters
             if(fileName.contains("..")) {
                 throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
             }
-
-            // Copy file to the target location (Replacing existing file with the same name)
             Path targetLocation = this.fileStorageLocation.resolve(fileName);
             Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
-
             return fileName;
+
         } catch (IOException ex) {
             throw new FileStorageException("Could not store file " + fileName + ". Please try again!", ex);
         }
     }
-
-    public Resource loadFileAsResource(String fileName) {
-        try {
-            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
-            Resource resource = new UrlResource(filePath.toUri());
-            if(resource.exists()) {
-                return resource;
-            } else {
-                throw new MyFileNotFoundException("File not found " + fileName);
-            }
-        } catch (MalformedURLException ex) {
-            throw new MyFileNotFoundException("File not found " + fileName, ex);
-        }
-    }
-    
 }
